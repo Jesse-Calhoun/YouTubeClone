2c1b52b82de5df22451bad777c6926dfcd7e3a1f
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-17 16:17:15 -0500

    Added to SearchBar componenet.

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 
 
-const SearchBar = ({}) => {
+const SearchBar = ({videos, getVideoResults, setSearchTerms, searchTerms}) => {
+    
+
+    function handleSearch(){
+        event.preventDefault();
+        let newVideoResults = {
+            title,
+            video_id,
+        }
+        setSearchTerms()
+        getVideoResults()
+    }
+
     return ( 
-        <div>
-            <input type='search' />
-            <button type='submit'>Search</button>
+        <div >
+            <input type='search' onCh />
+            <button type='submit' onClick={pass}>Search</button>
         </div>
      );
 }
