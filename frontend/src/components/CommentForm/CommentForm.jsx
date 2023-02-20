114c27c4960467c6c4f62a9d6a08f7f06892eefc
unknown <schuldjack@gmail.com>
2023-02-20 14:54:13 -0800

    Changed comment from object to string

--- frontend/src/components/CommentForm/CommentForm.jsx
+++ frontend/src/components/CommentForm/CommentForm.jsx
@@ -5,7 +5,7 @@ import useAuth from "../../hooks/useAuth";
 const CommentForm = ({ videoId }) => {
 
   const { token } = useAuth();
-  const [comment, setComment] = useState({})
+  const [comment, setComment] = useState('')
     
   async function addNewComment(newComment) {
     let url = "http://127.0.0.1:8000/api/" + videoId + "comments/add_comment/";
