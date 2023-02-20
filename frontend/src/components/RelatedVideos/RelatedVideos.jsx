18747007936905788771b7d9b0d66e7046bf2edb
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-20 14:15:22 -0500

    Have related videos displaying.

--- frontend/src/components/RelatedVideos/RelatedVideos.jsx
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -9,17 +9,18 @@ const RelatedVideos = ({ relatedVideos, selectedVideo, setSelectedVideo, videoPa
             {relatedVideos.map((relatedVideo)=>{
                 let { url, width, height } = relatedVideo.snippet.thumbnails.default;
 
-                // setVideoPageUrl(`/watch/${relatedVideo.id.videoId}`);
+                setVideoPageUrl(`/watch/${relatedVideo.id.videoId}`);
                 
                 function handleRelatedVideo(){
                     setSelectedVideo(relatedVideo);
                 }
-            
 
-            <Link to={`/watch/${relatedVideo.id.videoId}`} onClick={handleRelatedVideo} >
-                <p>{relatedVideo.snippet.title}</p>
-                <iframe width={width} height={height} src={url}></iframe>
-            </Link>
+                return (
+                    <Link to={videoPageUrl} onClick={handleRelatedVideo} >
+                        <p>{relatedVideo.snippet.title}</p>
+                        <iframe width={width} height={height} src={url}></iframe>
+                    </Link>
+                )
             })}
         </div>
      );
